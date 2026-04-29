2f651a970cdc623a88b768c3453e4cd27f42bd72
VEDANG RAUT <vedang.raut@nmiet.edu.in>
2026-04-29 07:13:06 +0530

    Delete admission_project directory

--- admission_project/admission_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AdmissionAppConfig(AppConfig):
-    name = "admission_app"
--- admission_project/admission_app/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Admission
-
-class AdmissionForm(forms.ModelForm):
-    class Meta:
-        model = Admission
-        fields = '__all__'
